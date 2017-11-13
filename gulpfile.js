0af0454dcad1afbccf0dff42d4192068223e0814
Aiman Abdel-Samad <aiman.samad@zemoga.com>
2017-11-13 01:31:59 -0500

    updated gulpfile, packages, sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,37 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    sync = require('browser-sync').create();
 
-gulp.task('default', function() {
-  // place code for your default task here
-  gulp.src('client/app/*.*')
-    .pipe(gulp.dest('./dist/'))
+gulp.task('default', ['base:html', 'base:sass', 'base:watch', 'default:server']);
+
+gulp.task('base:html', function(){
+  gulp.src('app/*.*')
+      .pipe(gulp.dest('dist/'))
+});
+
+gulp.task('base:sass', function () {
+  return  gulp.src('app/sass/**/*.scss')
+              .pipe(sass().on('error', sass.logError))
+              .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('base:vendor', function () {
+  return  gulp.src(['node_modules/normalize.css/normalize.css'])
+              .pipe(concat('vendor.css'))
+              .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('base:watch', function () {
+  gulp.watch('app/sass/**/*.scss', ['base:sass']);
+  gulp.watch('app/**/*.html', ['base:html']);
+});
+
+//not working yet:
+gulp.task('default:server', function () {
+  sync.init({
+      server: {
+        baseDir: "dist"
+      }
+  });
 });
